b79e11d0d2cfae8d58d2bcf639a639d33b007b8c
lleal24 <lorenaleal24@gmail.com>
2019-09-15 17:58:59 -0700

    Rediseño de pagina principal con grid

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
 var boton = document.getElementById("btn1");
+var boton2 = document.getElementById("btn2");
+var listaTarea = document.getElementById("lista");
+var item;
 
-function registrar() {
-    var nombre = document.getElementById("nombre").value; 
+var nombre;
+
+/* function registrar() {
+    nombre = document.getElementById("nombre").value; 
     var apellido = document.getElementById("apellido").value; 
+    localStorage.setItem("lastname", nombre);
     alert("Registro exitoso: " + nombre);
-    location = "www.google.com";
+    location.href="pages/perfil.html";    
+}
+function cargar() {
+    document.getElementById("user").innerHTML = nombre;
+} */
+
+function agregarTarea() {
+    let tarea = document.getElementById("tarea").value;
+    let lista = document.createElement("li");
+    lista.textContent = tarea;
+    lista.addEventListener('click', remover);
+    listaTarea.appendChild(lista);
+   
     
 }
-boton.onclick = function () {
+
+
+
+/* boton.onclick = function () {
     registrar();
+} */
+boton2.onclick = function () {
+    agregarTarea();
 }
